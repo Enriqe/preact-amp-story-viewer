07dd07399ceadd815b6e3529c455d126e6ddc59e
Enrique Marroquin <artezan@google.com>
2019-12-26 13:08:15 -0600

    add react hook warnings

--- src/amp-story-embed.js
+++ src/amp-story-embed.js
@@ -16,11 +16,9 @@
 
 const {useEffect, useRef} = preactHooks;
 
-/**
- *
- * @param {*} props
- * @return {*}
- */
+let currentIdx = 0;
+let maxIdx = 0;
+
 export function AmpStoryEmbed(props) {
   props['decoding'] = 'async';
 
@@ -28,44 +26,52 @@ export function AmpStoryEmbed(props) {
   const containerRef = useRef();
   const container = buildStories(children, containerRef);
   const messagingMap = {};
+  maxIdx = children.length - 1;
 
   useEffect(() => {
     if (!containerRef.current) {
       return;
     }
-    Array.prototype.forEach.call(containerRef.current.children, (iframe) => {
+    const storyIframes = containerRef.current.children;
+    Array.prototype.forEach.call(storyIframes, (iframe) => {
       initializeHandshake(iframe).then((messaging) => {
         messagingMap[iframe.id] = messaging;
       });
     });
 
-    const currentIframe = containerRef.current.children[0];
-    installGestures(messagingMap, currentIframe);
+    installGestures(messagingMap, storyIframes);
   });
 
   return preact.createElement('viewer', {}, container);
 }
 
 /**
- *
+ * Installs gestures.
  * @param {!Object} messagingMap
- * @param {!Element} iframe
+ * @param {!Element} storyIframes
  */
-function installGestures(messagingMap, iframe) {
+function installGestures(messagingMap, storyIframes) {
   const buttonLeft = window.document.querySelector('button.prev');
   const buttonRight = window.document.querySelector('button.next');
-  // TODO: install swiping gestures for real.
+
+  // TODO: install swiping gestures.
   buttonLeft.addEventListener('click', () => {
-    previousStory(messagingMap, iframe);
+    currentIdx = Math.max(currentIdx - 1, 0);
+    pauseStory(messagingMap, storyIframes[currentIdx + 1]);
+    displayStory(messagingMap, storyIframes[currentIdx]);
+    preRenderStory(messagingMap, storyIframes[currentIdx - 1]);
   });
   buttonRight.addEventListener('click', () => {
-    nextStory(messagingMap, iframe);
+    currentIdx = Math.min(currentIdx + 1, maxIdx);
+    pauseStory(messagingMap, storyIframes[currentIdx - 1]);
+    displayStory(messagingMap, storyIframes[currentIdx]);
+    preRenderStory(messagingMap, storyIframes[currentIdx + 1]);
   });
 }
 
 /**
- *
- * @param {*} stories
+ * Builds iframes where stories will appear.
+ * @param {!Array<!Element>} stories
  * @param {*} containerRef
  * @return {*}
  */
@@ -73,11 +79,13 @@ function buildStories(stories, containerRef) {
   const iframes = stories.map((story, idx) => {
     const url =
       story.props.href +
-      `?amp_js_v=0.1#visibilityState=prerender&width=412&height=660&paddingTop=50&prerenderSize=1&origin=http%3A%2F%2F127.0.0.1%3A8080`;
+      `?amp_js_v=0.1#visibilityState=inactive&width=412&height=660&paddingTop=50&prerenderSize=1&origin=http%3A%2F%2F127.0.0.1%3A8080`;
     const iframe = preact.createElement('iframe', {
       id: 'AMP_DOC_' + idx,
       src: url,
+      style: 'transform: translateX(1000%)',
     });
+
     return iframe;
   });
 
@@ -100,12 +108,15 @@ function initializeHandshake(iframe) {
           console.log('default', {handler});
         });
         // Render first story.
+        // TODO: move this elsewhere and reuse displayStory.
         if (iframe.id === 'AMP_DOC_0') {
           messaging.sendRequest('visibilitychange', {state: 'visible'}, true);
+          iframe.style.transform = 'translateX(0)';
         }
         messaging.registerHandler('moreInfoLinkUrl', () => {
           return Promise.resolve('https://www.google.com');
         });
+        console.log('finished handshake', iframe.id);
         return messaging;
       },
       (err) => {
@@ -115,31 +126,60 @@ function initializeHandshake(iframe) {
 }
 
 /**
- *
- * @param {*} messagingMap
- * @param {*} iframe
+ * Renders story visible.
+ * @param {!Object} messagingMap
+ * @param {!Element} iframe
  */
-function nextStory(messagingMap, iframe) {
-  // pause and hide current story
-  // play next story
-  // pre-render next-next story
-  if (!messagingMap[iframe.id]) {
+function displayStory(messagingMap, iframe) {
+  if (!iframe || !messagingMap[iframe.id]) {
     return;
   }
-  console.log('next');
+
+  console.log('displaying: ', iframe.id);
+  iframe.style.transform = 'translateX(0)';
+  messagingMap[iframe.id].sendRequest(
+      'visibilitychange',
+      {state: 'visible'},
+      true,
+  );
 }
 
 /**
- *
- * @param {*} messagingMap
- * @param {*} iframe
+ * Pauses story.
+ * @param {!Object} messagingMap
+ * @param {!Element} iframe
  */
-function previousStory(messagingMap, iframe) {
-  // pause and hide current story
-  // play previous story
-  // pre-render previous-previous story
-  if (!messagingMap[iframe.id]) {
+function pauseStory(messagingMap, iframe) {
+  if (!iframe || !messagingMap[iframe.id]) {
     return;
   }
-  console.log('prev');
+
+  console.log('pausing: ', iframe.id);
+  iframe.style.transform = 'translateX(200%)';
+  messagingMap[iframe.id].sendRequest(
+      'visibilitychange',
+      {state: 'paused'},
+      true,
+  );
+}
+
+/**
+ * Prerenders story.
+ * @param {!Object} messagingMap
+ * @param {!Element} iframe
+ */
+function preRenderStory(messagingMap, iframe) {
+  if (!iframe || !messagingMap[iframe.id]) {
+    return;
+  }
+
+  console.log('prerendering: ', iframe.id);
+  iframe.style.transform = 'translateX(100%)';
+  messagingMap[iframe.id].sendRequest(
+      'visibilitychange',
+      {
+        state: 'prerender',
+      },
+      true,
+  );
 }
